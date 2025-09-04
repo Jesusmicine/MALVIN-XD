ff709dca0f8e450418c5e52c765fe0bc6c3998ee
Jesusmicine <165343956+Jesusmicine@users.noreply.github.com>
2025-09-04 07:35:07 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -6,5 +6,5 @@ module.exports = {
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "africa/douala" //put your country timeZone....leave blank if u don't know.
 };
