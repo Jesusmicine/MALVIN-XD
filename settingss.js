1df661f61f77063e21af27c78d70218af279bf3d
Jesusmicine <165343956+Jesusmicine@users.noreply.github.com>
2025-09-04 07:28:24 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~cj1x1DyZ#w0G8bRpiFgfDtAmHJbPztReoeNUZxTPcWrE7XiMQMYw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "237672620967", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "" //put your country timeZone....leave blank if u don't know.
 };
